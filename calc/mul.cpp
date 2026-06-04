5720b290d464b2e915a9900d6a532a4501e68273
GreenGrass <spatiumdog@gmail.com>
2026-06-04 10:59:28 +0900

    modify - calc.cpp - mod.cpp - nul.cpp

--- calc/mul.cpp
+++ calc/mul.cpp
@@ -1,6 +1,37 @@
 #include "mul.h"
+#include "add.h"
+
+int abs(int x)
+{
+	if (x < 0)
+	{
+		return -x;
+	}
+	return x;
+}
 
 int mul(int x, int y)
 {
-	return 0;
+	bool minus = false;
+	int result = 0;
+	if ((x < 0) ^ (y < 0))
+	{
+		minus = true;
+	}
+	x = abs(x);
+	y = abs(y);
+
+	for (int i = 0; i < x; i++)
+	{
+		result = add(result, y);
+	}
+
+	if (minus)
+	{
+		return -result;
+	}
+	else
+	{
+		return result;
+	}
 }
